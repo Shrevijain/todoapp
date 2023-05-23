0422ddd156980589b1192828e173b3c159db8d54
Shrevijain <shrevijainbhl@gmail.com>
2023-05-23 15:52:35 +0530

    ToDoReactApp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// import {Demo} from './components/Demo';
+// import Demo1 from './components/Demo1';
+// import Parent from './components/Parent';
+//  import Demo2 from './components/Demo2';
+import ToDoList from './components/ToDoList';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTrash, faPlus, faEdit } from "@fortawesome/free-solid-svg-icons";
+library.add(faTrash, faEdit, faPlus);
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <Demo></Demo>
+      <Demo1></Demo1> */}
+      {/* <Parent ></Parent> */}
+      {/* <Demo2></Demo2> */}
+      <ToDoList></ToDoList>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +31,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ src/components/Demo.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+
+export class Demo extends Component {
+    render(){
+       return(
+        <div>
+             <h1>Hello Duniya!</h1>
+        </div>
+       )
+    }
+}
+
